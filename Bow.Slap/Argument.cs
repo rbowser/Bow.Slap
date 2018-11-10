cb127fb474964f1dee968b096d52952cb13e45a4
Rob Bowser <robert.d.bowser@gmail.com>
2018-11-09 23:57:22 -0500

    Add Argument type and simple type utility

--- Bow.Slap/Argument.cs
+++ Bow.Slap/Argument.cs
@@ -1,8 +1,52 @@
 ﻿using System;
+using System.Linq;
 
 namespace Bow.Slap
 {
-    public class Argument
+    public class Argument<T>
     {
+        private string _name = string.Empty;
+        private string _short = string.Empty;
+        private string _long = string.Empty;
+        private string _description = string.Empty;
+        private Func<T, bool> _isValid = _ => true;
+
+        public Argument(string name)
+        {
+            if (!Utils.IsSimpleType(typeof(T)))
+                // TODO better error message
+                throw new InvalidOperationException($"Arguments do not support {typeof(T)}.");
+            _name = name;
+        }
+
+        public Argument<T> Description(string arg)
+        {
+            _description = arg;
+            return this;
+        }
+
+        public Argument<T> Long(string arg)
+        {
+            _long = arg;
+            return this;
+        }
+
+        public Argument<T> Short(string arg)
+        {
+            _short = arg;
+            return this;
+        }
+
+        public Argument<T> Values(params T[] values)
+        {
+            _isValid = arg => values.Any(v => v.Equals(arg));
+            return this;
+        }
+
+        public Argument<T> ValidityCheck(Func<T, bool> isValid)
+        {
+            _isValid = isValid;
+            return this;
+        }
     }
 }
---
+++ Bow.Slap/Utils.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bow.Slap
+{
+    class Utils
+    {
+        static bool IsSimpleType(Type t)
+        {
+            return t.IsPrimitive
+                || t == typeof(string)
+                || t == typeof(DateTime)
+                || t == typeof(DateTimeOffset)
+                || t == typeof(decimal)
+                || t == typeof(TimeSpan);
+        }
+    }
+}
